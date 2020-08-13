eece65c78b1d6ed7230799168a2e57ddaa842150
ibchand <ichand@bu.edu>
2020-08-13 14:00:57 -0400

    Removed PA1

--- PA1/P1/PA1P1.h
+++
@@ -1,6 +0,0 @@
-#ifndef PA1P1_H
-#define PA1P1_H
-    double let2num(std::string classX);
-    double calcGPA(double class1, double class2, double class3, double class4, double class5);
-    bool errorCheck(std::string);
-#endif
